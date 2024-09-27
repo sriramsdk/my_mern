b2f79e428a6435b44bbeee70cfcfea6c7d85b0d5
Sriram.R <146877320+sriramsdk@users.noreply.github.com>
2024-09-27 22:23:32 +0530

    added another url for live

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'https://www.my_mern.com',
     'https://my_mern.com',
     'visiting-lura-sdkgroup-184d32b4.koyeb.app/',
-    'my-mern.visiting-lura.internal:8000'
+    'my-mern.visiting-lura.internal:8000',
+    'https://sdk-mern.netlify.app/'
 ]
 
 module.exports = allowedOrigins
