57ca4d6f11c7e899bc0feb2f60551c6ac222dfe3
Sriram.R <146877320+sriramsdk@users.noreply.github.com>
2024-09-27 22:27:27 +0530

    allowed origins changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,9 +3,9 @@ const allowedOrigins = [
     'http://localhost:3500',
     'https://www.my_mern.com',
     'https://my_mern.com',
-    'visiting-lura-sdkgroup-184d32b4.koyeb.app/',
+    'visiting-lura-sdkgroup-184d32b4.koyeb.app',
     'my-mern.visiting-lura.internal:8000',
-    'https://sdk-mern.netlify.app/'
+    'https://sdk-mern.netlify.app'
 ]
 
 module.exports = allowedOrigins
